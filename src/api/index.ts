852ca74e49e290f0c9ebb393f84a7f3eefe0c842
JoanLinares <joan.linares@gracia.lasalle.cat>
2025-02-10 19:56:43 +0100

    feat: connection with mongo and supabase, maded register

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import authRouter from './routers/authRouter.js';
 
 
 const apiRouter = Router();
@@ -6,5 +7,7 @@ const apiRouter = Router();
 // Per obtenir el body en format json
 apiRouter.use(express.json());
 
+// Agregar rutas de autenticación
+apiRouter.use('/auth', authRouter);
 
 export default apiRouter;
--- src/api/models/Joi/authSchemas.ts
+++ src/api/models/Joi/authSchemas.ts
@@ -1,9 +1,10 @@
 import Joi from "joi";
 
 const registerSchema = Joi.object({
-  email: Joi.string().email().required(),
+  correo: Joi.string().email().required(),
+  username: Joi.string().min(3).max(30).required(),
   password: Joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,15}$/).required(),
-  passwordConfirm: Joi.string().valid(Joi.ref('password')).required(),
+  repeatPassword: Joi.string().valid(Joi.ref('password')).required(),
 });
 
-export { registerSchema }
+export { registerSchema };
