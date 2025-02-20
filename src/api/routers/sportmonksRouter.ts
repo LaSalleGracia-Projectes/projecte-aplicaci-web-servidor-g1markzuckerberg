a8354353627f02a88d041dc4dc6fbe27bfc08cc6
albertgarrido4 <algarri2000@gmail.com>
2025-02-20 18:51:39 +0100

    perf: :recycle: Router

--- src/api/routers/sportmonksRouter.ts
+++ src/api/routers/sportmonksRouter.ts
@@ -1,49 +1,19 @@
 import { Router } from 'express';
-import { getAllPlayersFromTeams } from '../../services/PlayerService.js';
-import { getFixturesByRoundNumber, getCurrentRounds } from '../../services/FixturesService.js';
+import { getAllPlayersController, getFixturesByRoundNumberController, getCurrentRoundsController, processRoundFantasyPointsController } from '../controllers/sportmonksController.js';
 
 const sportmonksRouter = Router();
 
 // Ruta para obtener todos los jugadores
-sportmonksRouter.get('/allPlayers', getAllPlayersFromTeams);
+sportmonksRouter.get('/allPlayers', getAllPlayersController);
 
-// Nueva ruta: Obtener jornadas según el número proporcionado
-sportmonksRouter.get('/jornadas/:roundNumber', async (req, res) => {
-    const { roundNumber } = req.params; // Capturar el parámetro de la URL
+// Ruta para obtener fixtures por jornada (según el roundNumber)
+sportmonksRouter.get('/jornadas/:roundNumber', getFixturesByRoundNumberController);
 
-    try {
-        const fixtures = await getFixturesByRoundNumber(roundNumber);
-        res.status(200).json({
-            jornada: roundNumber,
-            fixtures,
-        });
-    } catch (error) {
-        console.error('Error al obtener las jornadas:', error);
-        res.status(500).json({
-            error: 'Error al obtener las jornadas',
-            message: error instanceof Error ? error.message : 'Error desconocido',
-        });
-    }
-});
-sportmonksRouter.get('/jornadaActual', async (req, res) => {
-    try {
-        const rounds = await getCurrentRounds();
-        if (rounds && rounds.length > 0) {
-            res.status(200).json({
-                jornadaActual: rounds,
-            });
-        } else {
-            res.status(404).json({
-                message: 'No se encontraron jornadas actuales.',
-            });
-        }
-    } catch (error) {
-        console.error('Error al obtener la jornada actual:', error);
-        res.status(500).json({
-            error: 'Error al obtener la jornada actual',
-            message: error instanceof Error ? error.message : 'Error desconocido',
-        });
-    }
-});
+// Ruta para obtener la jornada actual
+sportmonksRouter.get('/jornadaActual', getCurrentRoundsController);
+
+// Ruta para procesar la ronda completa y calcular los puntos fantasy
+sportmonksRouter.get('/processRoundFantasyPoints/:roundId', processRoundFantasyPointsController);
+
 
 export default sportmonksRouter;
