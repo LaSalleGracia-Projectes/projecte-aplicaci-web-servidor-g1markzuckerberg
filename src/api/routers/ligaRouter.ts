b112ce1cc9f4609fffd6054aa8241f9acb85d298
JoanLinares <joan.linares@gracia.lasalle.cat>
2025-02-23 13:36:56 +0100

    feat: Fixed unirse a liga and maded services for usuarios_ligas endpoint to getAllusersLiga with jornada

--- src/api/routers/ligaRouter.ts
+++ src/api/routers/ligaRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { createLiga, joinLiga } from '../controllers/ligaController.js';
+import { createLiga, joinLiga, getUsersByLiga } from '../controllers/ligaController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const ligaRouter = Router();
 
 ligaRouter.post('/create', authMiddleware, createLiga);
-ligaRouter.get('/join/:ligaCode', authMiddleware, joinLiga); // Ahora se requiere autenticación
+ligaRouter.post('/join/:ligaCode', authMiddleware, joinLiga); // Ahora se requiere autenticación
+// Example: /api/v1/ligas/users/ABC123?jornada_id=22
+ligaRouter.get('/users/:ligaCode', authMiddleware, getUsersByLiga);
 
 export default ligaRouter;
