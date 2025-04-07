2aa1989de29b7c3e60c5d5101ea33681c1b3d598
albertgarrido4 <algarri2000@gmail.com>
2025-04-08 01:26:30 +0200

    refactor: :chart_with_upwards_trend: Update fixtures response

---
+++ src/types/FixtureExtended.ts
@@ -0,0 +1,9 @@
+interface FixtureExtended {
+    id: number;
+    name: string;
+    result_info: string;
+    starting_at_timestamp: number;
+    local_team_image: string;
+    visitant_team_image: string;
+}
+export default FixtureExtended;
